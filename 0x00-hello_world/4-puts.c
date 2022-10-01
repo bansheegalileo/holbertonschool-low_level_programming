90d9d85cfffe9991cc018531eb4859f21a86adb9
bansheegalileo <venable.rin@gmail.com>
2022-09-30 20:59:25 -0500

    4.1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+	* main - Prints puzzle message
+	*
+	* Return: Always 0 (Success)
+	*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
