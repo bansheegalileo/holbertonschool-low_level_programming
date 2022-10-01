6be8f39d7f4dc974928d9f6c1b371568f7160ec4
bansheegalileo <venable.rin@gmail.com>
2022-10-01 00:06:06 -0500

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - aye bee see dee eee eff jee ayech eye jay kay ell emm
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
