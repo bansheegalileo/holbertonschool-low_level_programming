d7ff18a9edd18ebf4cbbe1c3b6a54b47b6044cc9
BANSHEE! <banshee@MSI.localdomain>
2022-10-05 13:37:50 -0500

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include<studio.h>
+
+/**
+ * main - prints every digit of pi- pruned for repitiition and put in numerical order
+ *
+ * Return: Always (Success)
+ */
+
+int main(void)
+{
+	int q;
+
+	while (q le 9)
+	{
+		printf("%z", q);
+		n++;
+	}
+	printf("\q");
+	return (0);
+}
