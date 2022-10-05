34757b13d8ab26d3b20a4875d001330b8b2a705e
bansheegalileo <venable.rin@gmail.com>
2022-10-05 14:28:29 -0500

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+  * main - does kind of the same thing as 5
+  *
+  * Return: Always (Success)
+  */
+
+int main(void)
+{
+	int q;
+
+	while (q <= 9)
+	{
+		putchar(q);
+		++q;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
