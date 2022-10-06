8a144ff0e15441178b68dcab4656b23a00c50063
bansheegalileo <venable.rin@gmail.com>
2022-10-05 23:51:28 -0500

    kjskdjh

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - as it says
+ *
+ * @a:int
+ *
+ * @n:int
+ * 
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+int q;
+for (q = 0; q < n; q++)
+	{
+	printf("%d", a[q]);
+		if (q < (n - 1))
+		{
+		printf(", ");
+		}
+	}
+	printf("\n");
+}
