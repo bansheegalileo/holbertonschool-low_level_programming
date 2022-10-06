45a7b3514df3d418ec9a556aaeb7d2c71238f5df
bansheegalileo <venable.rin@gmail.com>
2022-10-05 19:29:57 -0500

    fdfghjyghv

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - is it alphabet?
+ * 
+ *@c: char
+ *
+ * Return: 1 if true, 0 if not
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122 && c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
