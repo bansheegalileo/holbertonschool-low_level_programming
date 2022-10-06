1846aab4be2254bec763e3953f287f8ff10cb06c
bansheegalileo <venable.rin@gmail.com>
2022-10-05 23:35:03 -0500

    msge

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse
+ *
+ * @s: s.p.i.
+ *
+ * Return: 0
+*/
+0
+void rev_string(char *s)
+{
+	int a, b;
+	char c;
+
+	for (a = 0; s[a] != '\0'; ++a)
+		;
+
+	for (b = 0; b < a / 2; ++b)
+	{
+		c = s[b];
+		s[b] = s[a - 1 - b];
+		s[a - 1 - b] = ch;
+	}
+
+}
