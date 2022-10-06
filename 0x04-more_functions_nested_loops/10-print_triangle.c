1a36e251bb3379cb8ae3f757fd9ce317aa5b43ab
bansheegalileo <venable.rin@gmail.com>
2022-10-05 23:09:08 -0500

    Rush time

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+  * print_triangle - threeeeeee sides babeeeeey
+  *
+  * @size: sizangle
+  *
+  * Return: 0
+  */
+
+void print_triangle(int size)
+{
+	int a, b, c;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = size - a; b > 1; b--)
+			{
+				_putchar(32);
+			}
+
+			for (c = 0; c <= x; c++)
+			{
+				_putchar(35);
+			}
+
+			_putchar('\n');
+		}
+	}
+}
