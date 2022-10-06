bdcdea026db98e996aeba5441e96fec9804dcbaf
bansheegalileo <venable.rin@gmail.com>
2022-10-05 22:43:01 -0500

    kjdkfjghkfdjg

--- 0x04-more_functions_nested_loops/6-print.line.c
+++ 0x04-more_functions_nested_loops/6-print.line.c
@@ -11,18 +11,13 @@
 void print_line(int n)
 {
 	int l;
+
+	if (n > 0)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		
-		else
-		for (l = 1; l <= n; ++l)
+		for (l = 0; l < n; l++)
 		{
 			_putchar('_');
-			_putchar('\n');
 		}
 	}
+	_putchar('\n');
 }
-
