346ab5916bc99c0c3fa1cfc90e830b855a520afd
bansheegalileo <venable.rin@gmail.com>
2022-10-05 20:52:17 -0500

    kjhgf

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * print_to_98 - from n to 98
+ *
+ *@n: int
+ *
+ * Return : various
+ */
+
+void print_to_98(int n)
+{
+	while (n <=98)
+	{
+		printf("%i, ", n);
+		if (n > 98)
+		{
+			n--;
+		}
+
+		else
+		{
+			n++;
+		}
+	}
+		printf("98\n);
+}
