dadcdb45d7d38d733eac6f294c3bce274218a6ca
bansheegalileo <venable.rin@gmail.com>
2022-10-05 20:20:17 -0500

    kjkhj

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,17 +13,18 @@ void jack_bauer(void)
 
 	for (h = 0; h <= 23;)
 	{
+		h++;
+
 		for (m = 0; m <= 59;)
 		{
+			m++;
 			_putchar((h / 10) + 0);
 			_putchar((h % 10) + 0);
 			_putchar(':');
 			_putchar((m / 10) + 0);
 			_putchar((m % 10) + 0);
-			m++;
+			_putchar('\n');
 		}
 
-		h++;
-		_putchar('\n');
 	}
 }
