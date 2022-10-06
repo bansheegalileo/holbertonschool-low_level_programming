6b2d39f7b6dd6aa06fe029a4f57f605ccafa652a
bansheegalileo <venable.rin@gmail.com>
2022-10-05 21:58:46 -0500

    jhjghgfdf

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - 0-9, new line
+ *
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+int i;
+
+	for (i = 48; i <= 57;)
+	{
+		_putchar(i);
+		i++;
+	}
+
+_putchar('\n');
+}
