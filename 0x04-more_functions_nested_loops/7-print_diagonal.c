768eb977af0c5830ec33e2d029a6bbe4d8cdd7bb
bansheegalileo <venable.rin@gmail.com>
2022-10-05 22:50:29 -0500

    kjkrjhg

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_diagonal - diag
+ *
+ * @n:int
+ *
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int d;
+        int l;
+
+	if (n > 0)
+	{
+		for (d = 0; d < n; d++)
+		{	
+			for (l = 0; l < i; l++)
+			{
+				_putchar(' ');
+			}
+
+		_putchar('\\');
+
+		_putchar('\n');
+
+		}
+	}
+
+	else
+	{
+		_putchar('\n');
+	}
+}
