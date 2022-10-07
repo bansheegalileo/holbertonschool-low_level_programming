6df0f3ff061682eac5597b998bb4195b736e3946
bansheegalileo <venable.rin@gmail.com>
2022-10-06 23:46:26 -0500

    afafsfd

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - 2diagsums
+ *
+ * @a: int
+ * 
+ * @size: int size
+ *
+ * Return: 0
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int q, s1 = 0, s2 = 0;
+
+	for (q = 0; q < size; ++q)
+	{
+		s1 += a[q];
+		a += size;
+	}
+
+	a -= size;
+
+	for (q = 0; q < size; ++q)
+	{
+		s2 += a[q];
+		a -= size;
+	}
+
+	printf("%d, %d\n", s1, s2);
+}
