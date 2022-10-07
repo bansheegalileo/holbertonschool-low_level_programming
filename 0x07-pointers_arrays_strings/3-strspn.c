00bb94b27101d78dac9f3746d1b53ee76ea9a729
bansheegalileo <venable.rin@gmail.com>
2022-10-06 23:01:44 -0500

    ldskf

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strspn - pfx sbstr lngth
+ *
+ * @s: str
+ *
+ * @accept: str
+ *
+ * Return: unsgn int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int a = 0;
+	int b;
+
+	while (*s)
+	{
+		for (b = 0; accept[b]; ++b)
+		{
+			if (*s == accept[b])
+			{
+				++a;
+				break;
+			}
+
+			else if (accept[b + 1] == '\0')
+			{
+				return (a);
+			}
+		}
+
+		++s;
+	}
+
+	return (a);
+}
