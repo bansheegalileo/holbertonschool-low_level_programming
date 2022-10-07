9c216f75d723a83e704e96ed214b26245ade5487
bansheegalileo <venable.rin@gmail.com>
2022-10-06 21:45:15 -0500

    khksdjhfkdsf

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+ * cap_string - ALL WORDS
+ * 
+ * @s: str
+ *
+ * Return: saddress
+ */
+
+char *cap_string(char *s)
+{
+	int b, c;
+
+	b = 0;
+
+	char a[] = " \t\n,;.!?\"(){}";
+
+	while (*(s + b))
+	{
+		if (*(s + b) >= 'a' && *(s + b) <= 'z')
+		{
+			if (b == 0)
+				*(s + b) -= 'a' - 'A';
+			else
+			{
+				for (c = 0; c <= 12; ++c)
+				{
+					if (a[c] == *(s + b - 1))
+						*(s + b) -= 'a' - 'A';
+				}
+			}
+		}
+
+		++b;
+	}
+
+	return (s);
+}
