46790ff63997c699e1c58d46a5c3b512d86a6c82
bansheegalileo <venable.rin@gmail.com>
2022-10-06 22:43:05 -0500

    kjhgfy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memcpy - cp mem area
+ *
+ * @dest: ch
+ *
+ * @src: ch
+ *
+ * @n: int
+ *
+ * Return:ch ptr
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int q;
+
+	for (q = 0; q < n; ++q)
+	{	
+		dest[q] = src[q];
+	}
+
+	return (dest);
+}
