170aaaecf14d559fd35e2f805b2cbb387a8c997b
root <root@MSI>
2023-02-17 13:34:55 -0600

    Commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _puts - prints a string
+ * @str: string to print
+ */
+void _puts(char *str) {
+    while (*str != '\0') {
+        write(1, str, 1);
+        str++;
+    }
+    write(1, "\n", 1);
+}
+
+void _puts_recursion(char *s) {
+    if (*s == '\0') {
+        _puts("");
+        return;
+    }
+    _puts(s);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _puts_recursion("Puts with recursion");
+    return (0);
+}
