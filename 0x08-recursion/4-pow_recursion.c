52b94d82b1fb7b11f70467a60c7b22c6dfc9d2af
root <root@MSI>
2023-02-17 17:30:05 -0600

    oijoj

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- *_pow_recursion: finds x to the power of y with recursion
+ * _pow_recursion: finds x to the power of y with recursion
  *
  * @x: base number
  *
  * @y: exponent
  *
- * return: x to the power of y
+ * Return: x to the power of y
  */
 
 int _pow_recursion(int x, int y)
 {
-    if (y < 0)
+	if (y < 0)
 		{
 		return (-1);
-		} 
+		}
 	else if (y == 0)
 		{
 		return (1);
-		} 
+		}
 	else
 		{
 		return (x * _pow_recursion(x, y - 1));
