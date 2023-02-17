8efaa1198a051a1125e7eaa8f0438a002f8e42c7
root <root@MSI>
2023-02-17 14:55:48 -0600

    betttyyyyy

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,18 +2,17 @@
 #include "main.h"
 
 /**
- * 
  * _puts_recursion - recurses
- * 
+ *
  */
 
-void _puts_recursion(char *s) 
+void _puts_recursion(char *s)
 {
-    if (*s == '\0') 
-    {
-        _putchar('\n');
-        return;
-    }
-    _putchar(*s);
-    _puts_recursion(s + 1);
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
