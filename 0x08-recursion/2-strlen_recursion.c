ced3196c1a4adf1dae70c575b77ddb1c0ec8825d
root <root@MSI>
2023-02-17 16:47:49 -0600

    stringth

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _strlen_recursion - counts a string
+ *
+ * @s: string
+ *
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return 0;
+	}
+	return 1 + _strlen_recursion(s + 1);
+}
