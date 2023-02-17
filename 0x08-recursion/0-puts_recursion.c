10ef2ef63bb3e02f6f64aaa3ee27d5d87fcd3fdf
root <root@MSI>
2023-02-17 14:46:12 -0600

    for betty

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,16 @@
 #include <unistd.h>
 #include "main.h"
 
-void _puts_recursion(char *s) {
-    if (*s == '\0') {
+/**
+ * 
+ * _puts_recursion - recurses
+ * 
+ */
+
+void _puts_recursion(char *s) 
+{
+    if (*s == '\0') 
+    {
         _putchar('\n');
         return;
     }
