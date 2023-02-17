e4944c4765aa9a453c1e656d158b7e6a39e67378
bansheegalileo <109779435+bansheegalileo@users.noreply.github.com>
2023-02-17 14:25:20 -0600

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,22 +1,10 @@
 #include "main.h"
 #include <unistd.h>
 
-/**
- * _puts - prints a string
- * @str: string to print
- */
-void _puts(char *str) {
-    while (*str != '\0') {
-        write(1, str, 1);
-        str++;
-    }
-    write(1, "\n", 1);
-}
-
 void _puts_recursion(char *s) {
     if (*s == '\0') {
-        _puts("");
+        _putchar("");
         return;
     }
-    _puts(s);
+    _putchar(s);
 }
