89b678c40982462bb150be5da1e900040c80063f
root <root@MSI>
2023-02-17 20:38:48 -0600

    betty

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,33 +1,39 @@
 #include "main.h"
 
 /**
- * is_prime_number - is it prime?
+ * is_prime_number - checks if a number is prime
  *
- * is_div - is it divisible by a number between 2 and dv?
+ * @n: the number to check
  *
- * @n: base number
- *
- * @dv: the current divisor
- *
- * Return: 1 if prime, 0 otherwise
+ * Return: 1 if n is prime, 0 otherwise
  */
 
 int is_prime_number(int n)
 {
-    int is_div(int n, int dv);
+	int is_div(int n, int dv);
 
-    if (n < 2)
-        return (0);
-    else
-        return (is_div(n, 2));
+	if (n < 2)
+		return (0);
+	else
+		return (is_div(n, 2));
 }
+
+/**
+ * is_div - checks if n is divisible by a number between 2 and dv
+ *
+ * @n: base number
+ *
+ * @dv: the current divisor to check
+ *
+ * Return: 1 if divisible, 0 otherwise
+ */
 
 int is_div(int n, int dv)
 {
-    if (n % dv == 0)
-        return (0);
-    else if (dv * dv > n)
-        return (1);
-    else
-        return (is_div(n, dv + 1));
+	if (n % dv == 0)
+		return (0);
+	else if (dv * dv > n)
+		return (1);
+	else
+		return (is_div(n, dv + 1));
 }
