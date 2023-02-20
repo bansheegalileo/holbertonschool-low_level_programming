e7570534afb04d8e88a44c0a317875ca83820976
root <root@MSI>
2023-02-20 10:55:00 -0600

    isthisachg

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include "main.h"
 
-int main(int __attribute__((unused)) argc, char *argv[]) 
+/**
+ * main - prints name + \n
+ *
+ * @argc: args used
+ *
+ * @argv: arry points to args
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
 {
+	(void) argc;
 	printf("%s\n", argv[0]);
 	return 0;
 }
