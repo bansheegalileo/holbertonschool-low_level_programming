e1f68aec4b8492f1996866f6da495c547bb9e31e
root <root@MSI>
2023-02-20 15:30:04 -0600

    return value parentheses added

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,16 +13,16 @@
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int a, b, res;
 
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	result = num1 * num2;
-	printf("%d\n", result);
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	res = a * b;
+	printf("%d\n", res);
 	return (0);
 }
