826898f2cf0ad75aa540ea95e2f2e3eb97e1e451
root <root@MSI>
2023-03-06 13:33:12 -0600

    jdsd

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for a dog
+ *
+ * @d: pointer to a dog
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
