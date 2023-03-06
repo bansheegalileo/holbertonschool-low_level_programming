8a24737a2df41d1c9c66acd5367019f1401e5779
root <root@MSI>
2023-03-06 12:33:55 -0600

    eepy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,14 @@
-struct dog {
+/**
+ * struct dog - dogstructure
+ * @name: dogname
+ * @age: dogage
+ * @owner: dogowner
+ *
+ * Description: Represents a dog and 3 info pieces
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
