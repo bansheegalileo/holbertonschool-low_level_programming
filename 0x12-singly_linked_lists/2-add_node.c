0650cccef7ea4afd90a7ef918afe22cb731e2691
root <root@MSI>
2023-03-20 14:45:43 -0500

    pogogij

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node - new node to beginning
+ *
+ * @head: A pointer to the head of the list_t list.
+ *
+ * @str: The string to be added to the list_t list.
+ *
+ * Return: address of elem, Null on fail
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	char *argaxxdestroyerofworldsconquererofdeepspace;
+	int len;
+	list_t *new;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	argaxxdestroyerofworldsconquererofdeepspace = strdup(str);
+	if (argaxxdestroyerofworldsconquererofdeepspace == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	for (len = 0; str[len];)
+		len++;
+
+	new->str = argaxxdestroyerofworldsconquererofdeepspace;
+	new->len = len;
+	new->next = *head;
+
+	*head = new;
+
+	return (new);
+}
