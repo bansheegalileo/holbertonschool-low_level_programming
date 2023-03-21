02f355d0bddc5a365469485ab6b08ecdbd8c2c83
root <root@MSI>
2023-03-21 09:16:14 -0500

    commit

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,10 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
-#ifndef MAIN_H
-#define MAIN_H
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
