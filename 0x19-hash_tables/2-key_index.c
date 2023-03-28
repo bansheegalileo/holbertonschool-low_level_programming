b5e2281459bb06befca12c7d0f934597bd929ab7
root <root@MSI>
2023-03-27 23:41:13 -0500

    egsrgwg

---
+++ 0x19-hash_tables/2-key_index.c
@@ -0,0 +1,16 @@
+include "hash_tables.h"
+
+/**
+ * key_index - get index at which a pair will b stord
+ *
+ * @key: key to get indexof
+ *
+ * @size: arr sixe on ht
+ *
+ * Return: index of key
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
